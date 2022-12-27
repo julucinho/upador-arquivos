784d2e858a12b7fc5c59ee335617554b0fb7397a
J. Lúcio de Almeida Júnior <lucio.almeida@capitolio.tech>
2022-12-27 01:41:37 -0300

    fix: letting the SDK find its way to the credentials

--- upador-arquivo-plugins/src/main/java/org/julucin/upador_arquivo_plugins/use_cases/upload_file/s3_client/S3ClientInitializer.java
+++ upador-arquivo-plugins/src/main/java/org/julucin/upador_arquivo_plugins/use_cases/upload_file/s3_client/S3ClientInitializer.java
@@ -2,18 +2,18 @@ package org.julucin.upador_arquivo_plugins.use_cases.upload_file.s3_client;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.julucin.upador_arquivo_plugins.use_cases.upload_file.env_var_keys.UploadFilesUseCaseEnvVarRetriever;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+@Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class S3ClientInitializer {
 
     public static S3Client initializeClient(){
         return S3Client.builder()
                 .region(Region.of(UploadFilesUseCaseEnvVarRetriever.getAwsRegionEnvVar()))
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
     }
 
